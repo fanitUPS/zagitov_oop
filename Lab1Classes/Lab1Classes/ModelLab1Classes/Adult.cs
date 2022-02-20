a23220475c47be4ca3dccae834ceb98897697f18
Fanit <ffz2@tpu.ru>
2022-02-21 00:01:49 +0700

    features(Lab2Inheritance): create class adult and child

---
+++ Lab1Classes/Lab1Classes/ModelLab1Classes/Adult.cs
@@ -0,0 +1,61 @@
+﻿
+namespace ModelLab1Classes
+{
+    /// <summary>
+    /// Class adult
+    /// </summary>
+    class Adult : Person
+    {
+        /// <summary>
+        /// Passport ID of adult person
+        /// </summary>
+        private int _passportId;
+
+        /// <summary>
+        /// Marital status of person
+        /// </summary>
+        public MaritalStatus MaritalStatus { get; set; }
+
+        /// <summary>
+        /// Place of employment 
+        /// </summary>
+        private string _job;
+
+        /// <summary>
+        /// Passport ID of adult person
+        /// </summary>
+        public int PassportId
+        {
+            get => _passportId;
+            set => _passportId = value;
+        }
+
+        /// <summary>
+        /// Place of employment 
+        /// </summary>
+        public string Job
+        {
+            get => _job;
+            set => _job = value;
+        }
+
+        /// <summary>
+        /// Constructor of adult instance
+        /// </summary>
+        /// <param name="name">Name of adult</param>
+        /// <param name="surname">Surname of adult</param>
+        /// <param name="age">Age of adult</param>
+        /// <param name="gender">Gender of adult</param>
+        /// <param name="passportId">PassportId of adult</param>
+        /// <param name="maritalStatus">Martial status of person</param>
+        /// <param name="job">Job of person</param>
+        public Adult(string name, string surname, int age, PersonGender gender,
+            int passportId, MaritalStatus maritalStatus, string job)
+            : base(name, surname, age, gender)
+        {
+            PassportId = passportId;
+            MaritalStatus = maritalStatus;
+            Job = job;
+        }
+    }
+}
---
+++ Lab1Classes/Lab1Classes/ModelLab1Classes/Child.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ModelLab1Classes
+{
+    /// <summary>
+    /// Class child
+    /// </summary>
+    class Child : Person
+    {
+        /// <summary>
+        /// Mother of child
+        /// </summary>
+        private Adult _mother;
+
+        /// <summary>
+        /// Father of child
+        /// </summary>
+        private Adult _father;
+
+        /// <summary>
+        /// Name of kindergarten
+        /// </summary>
+        private string _kindergarten;
+
+        /// <summary>
+        /// Mother of child
+        /// </summary>
+        public Adult Mother
+        {
+            get => _mother;
+            set => _mother = value;
+        }
+
+        /// <summary>
+        /// Father of child
+        /// </summary>
+        public Adult Father
+        {
+            get => _father;
+            set => _father = value;
+        }
+
+        /// <summary>
+        /// Name of kindergarten
+        /// </summary>
+        public string Kindergarten
+        {
+            get => _kindergarten;
+            set => _kindergarten = value;
+        }
+
+        /// <summary>
+        /// Constructor of child instance
+        /// </summary>
+        /// <param name="name">Name of child</param>
+        /// <param name="surname">Surname of child</param>
+        /// <param name="age">Age of child</param>
+        /// <param name="gender">Gender of child</param>
+        /// <param name="mother">Mother of child</param>
+        /// <param name="father">Father of child</param>
+        /// <param name="kindergarten">Name of kindergarten</param>
+        public Child(string name, string surname, int age, PersonGender gender,
+            Adult mother, Adult father, string kindergarten)
+            : base(name, surname, age, gender)
+        {
+            Mother = mother;
+            Father = father;
+            Kindergarten = kindergarten;
+        }
+    }
+}
