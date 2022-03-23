fcf9e1d5ffe5d01cc5f79bc50198527c301332e5
Fanit <ffz2@tpu.ru>
2022-03-23 11:59:21 +0700

    fixed(<Lab4WinForms>): check git

--- Lab4WinForms/ViewLab4WinForms/ViewLab4WinForms/Program.cs
+++ Lab4WinForms/ViewLab4WinForms/ViewLab4WinForms/Program.cs
@@ -14,6 +14,7 @@ namespace ViewLab4WinForms
         [STAThread]
         static void Main()
         {
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
